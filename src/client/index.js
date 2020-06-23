7cfb8b3eea697cb5a840a0a12d4902a7321236a8
marina <35566244+marinakhamis@users.noreply.github.com>
2020-06-23 14:57:46 +0200

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
+
+alert("It's working !");
+console.log("Hello there, have a nice day !");
