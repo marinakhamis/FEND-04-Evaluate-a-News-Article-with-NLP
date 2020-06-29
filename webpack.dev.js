35ed1c776a5ad97a352ff54ff8db35f057d4e60d
marina <35566244+marinakhamis@users.noreply.github.com>
2020-06-29 10:55:04 +0200

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,20 +1,34 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+//Adding a reference to webpackto tell it to use HTML webpack plugin
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const loader = require('sass-loader')
 
 module.exports = {
+    //Setting up the starting place (Entry point)
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
-            {
+            {   //Getting webpack to use babel.
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                //Set up Sass with Webpack
+                //Note: don't use quotes here
+                test: /\.scss$/,
+                //Note: loaders are read from right to left
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        },
         ]
     },
     plugins: [
@@ -30,6 +44,6 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
     ]
 }
