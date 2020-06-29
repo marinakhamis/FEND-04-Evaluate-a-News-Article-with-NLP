09dec05c83c4bcea43f8beeb5abf7581148354f4
marina <35566244+marinakhamis@users.noreply.github.com>
2020-06-29 10:53:04 +0200

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,8 +2,13 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const textapi = require('./aylienAPI.js')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
 const app = express()
 app.use(express.static('dist'))
+app.use(cors())
+app.use(bodyParser.json())
 
 console.log(__dirname)
 
@@ -17,6 +22,16 @@ app.listen(2525, function () {
     console.log('Awesome app listening on port 2525!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+app.post('/api/call', function (req, res) {
+    // res.send(mockAPIResponse)
+    console.log(req.body)
+textapi.sentiment({
+        url: req.body.url,
+        mode: 'document' //Document mode is used in the case of long text 
+      }, function(error, response) {
+        if (error === null) {
+          console.log(response);
+          res.send(response);
+        }
+    });
 })
