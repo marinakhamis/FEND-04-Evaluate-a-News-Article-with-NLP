1c257798b7e30be68aa371f6a45ed64e05d5ab09
marina <35566244+marinakhamis@users.noreply.github.com>
2020-07-13 18:09:06 +0200

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,18 +6,32 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:2525/api/call', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        mode: 'cors',
-        body: JSON.stringify({url: formText})
-    })
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.text
-    })
+    if (Client.checkForName(formText)) {
+        
+        fetch('http://localhost:2525/api/call', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                mode: 'cors',
+                body: JSON.stringify({
+                    url: formText
+                })
+            })
+            .then(res => res.json())
+            .then(function(res) {
+                document.getElementById('results').innerHTML = res.polarity;
+                document.getElementById('text').innerHTML = res.text
+
+            })
+    } else {
+        if (!formText == '') {
+            alert('Enter Valid URL')
+        } else {
+            alert('Search Field is Empty')
+
+        }
+    }
 }
 
 export { handleSubmit }
